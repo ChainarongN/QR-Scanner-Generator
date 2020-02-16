191d50bad74e57305c445c4fe34f3ed4c6b49797
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-02-16 22:16:32 +0700

    upfile

---
+++ app/src/main/java/com/example/qrscannergenerator/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.qrscannergenerator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return v;
+    }
+}
