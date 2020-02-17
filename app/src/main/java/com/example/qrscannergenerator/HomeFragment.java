ab58ea022d965a2f9c19b46dba9b8bbdc7c5c946
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-02-17 17:41:07 +0700

    update 2

--- app/src/main/java/com/example/qrscannergenerator/HomeFragment.java
+++ app/src/main/java/com/example/qrscannergenerator/HomeFragment.java
@@ -4,19 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
 
     private View v;
 
+    private GridLayout gridLayout;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_home, container, false);
 
+        gridLayout = (GridLayout) v.findViewById(R.id.mainGrid);
+        setSingleEvent(gridLayout);
+
         return v;
     }
+
+    private void setSingleEvent(GridLayout gridLayout) {
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            CardView cardView = (CardView) gridLayout.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "Clicked at index " + finalI,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 }
