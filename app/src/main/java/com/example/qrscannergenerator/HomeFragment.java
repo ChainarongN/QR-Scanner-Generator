f9dec2adb92e46c9d7fb033806ed4f633121fd6a
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-02-19 14:11:23 +0700

    update

--- app/src/main/java/com/example/qrscannergenerator/HomeFragment.java
+++ app/src/main/java/com/example/qrscannergenerator/HomeFragment.java
@@ -1,14 +1,17 @@
 package com.example.qrscannergenerator;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.GridLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
@@ -24,6 +27,18 @@ public class HomeFragment extends Fragment {
 
         gridLayout = (GridLayout) v.findViewById(R.id.mainGrid);
         setSingleEvent(gridLayout);
+
+//        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getActivity().getWindow().setNavigationBarColor(ContextCompat.getColor(getContext(), R.color.bg_item));
+//        }
+
+//        getActivity().getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         return v;
     }
--- app/src/main/java/com/example/qrscannergenerator/ScannerFragment.java
+++ app/src/main/java/com/example/qrscannergenerator/ScannerFragment.java
@@ -8,10 +8,11 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SearchFragment extends Fragment {
+public class ScannerFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_scanner, container, false);
     }
 }
