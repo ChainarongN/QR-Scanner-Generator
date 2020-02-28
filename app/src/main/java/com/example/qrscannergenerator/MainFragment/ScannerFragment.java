30c48ff19d290c91715c8eb696d3795e882a3e37
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-02-28 14:30:13 +0700

    update scan

--- app/src/main/java/com/example/qrscannergenerator/MainFragment/ScannerFragment.java
+++ app/src/main/java/com/example/qrscannergenerator/MainFragment/ScannerFragment.java
@@ -2,40 +2,50 @@ package com.example.qrscannergenerator.MainFragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.qrscannergenerator.R;
+import com.example.qrscannergenerator.ScanCodeActivity;
 
 public class ScannerFragment extends Fragment {
 
     View v;
-    Button btn_share, btn_copy;
-    boolean check = false;
+    Button btn_share, btn_copy, btn_scanNew;
+
+    public static TextView result_qr;
+    private boolean check;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_scanner, container, false);
+        result_qr = (TextView) v.findViewById(R.id.result_qr);
+        btn_scanNew = (Button) v.findViewById(R.id.btn_scanNew);
 
+        scanNew_btn();
         check_btn();
-
 
         return v;
     }
 
 
-    private void check_btn(){
+    private void check_btn() {
         btn_share = (Button) v.findViewById(R.id.btn_share);
         btn_copy = (Button) v.findViewById(R.id.btn_copy);
 
-        if (!check) {
+        if (check == false) {
             btn_share.setBackgroundResource(R.drawable.bg_black);
             btn_copy.setBackgroundResource(R.drawable.bg_black);
 
@@ -71,9 +81,36 @@ public class ScannerFragment extends Fragment {
                     dialog.show();
                 }
             });
-        } else {
-            btn_share.setBackgroundResource(R.drawable.hover_bg3_bg2);
-            btn_copy.setBackgroundResource(R.drawable.hover_bg5_bg2);
         }
+    }
+
+    private void scanNew_btn() {
+
+        btn_scanNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getContext(), ScanCodeActivity.class));
+
+                result_qr.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                        check = true;
+                        btn_share.setBackgroundResource(R.drawable.hover_bg3_bg2);
+                        btn_copy.setBackgroundResource(R.drawable.hover_bg5_bg2);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable editable) {
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/qrscannergenerator/ScanCodeActivity.java
+++ app/src/main/java/com/example/qrscannergenerator/ScanCodeActivity.java
@@ -4,11 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScanCodeActivity extends AppCompatActivity {
+import com.example.qrscannergenerator.MainFragment.ScannerFragment;
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView ScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scan_code);
+        ScannerView = new ZXingScannerView(this);
+        setContentView(ScannerView);
+
+
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        if (result.getText() != null) {
+            ScannerFragment.result_qr.setText(result.getText());
+            onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        ScannerView.stopCamera();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        ScannerView.setResultHandler(this);
+        ScannerView.startCamera();
     }
 }
