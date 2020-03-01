a6c7d895b33da278be431bc6c7f8f153bf415a8c
Nueng <prince_nueng@hotmail.co.th>
2020-03-01 15:32:09 +0700

    update firstActivity

---
+++ app/src/main/java/com/example/qrscannergenerator/FirstActivity.java
@@ -0,0 +1,48 @@
+package com.example.qrscannergenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+        VideoView videoView = (VideoView) findViewById(R.id.simpleVideoView);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                },
+                5000);
+
+
+//        MediaController mediaController = new MediaController(this);
+//        videoView.setMediaController(mediaController);
+//        mediaController.setAnchorView(videoView);
+    }
+}
