8752b66cbabbcd814d51f5f9a6c945bdcbdc4514
Nueng <prince_nueng@hotmail.co.th>
2020-03-09 17:25:27 +0700

    update admob gen

--- app/src/main/java/com/example/qrscannergenerator/MainFragment/RateUsFragment.java
+++ app/src/main/java/com/example/qrscannergenerator/MainFragment/RateUsFragment.java
@@ -11,9 +11,15 @@ import androidx.fragment.app.Fragment;
 import com.example.qrscannergenerator.R;
 
 public class RateUsFragment extends Fragment {
+
+    private View v;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rate_us, container, false);
+        v = inflater.inflate(R.layout.fragment_rate_us, container, false);
+
+
+        return v;
     }
 }
