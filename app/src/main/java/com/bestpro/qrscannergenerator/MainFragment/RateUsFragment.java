9828147bb43b812a5afbed2369c3d09df44fce32
Nueng <prince_nueng@hotmail.co.th>
2020-04-22 13:29:48 +0700

    update

--- app/src/main/java/com/bestpro/qrscannergenerator/MainFragment/RateUsFragment.java
+++ app/src/main/java/com/bestpro/qrscannergenerator/MainFragment/RateUsFragment.java
@@ -2,11 +2,14 @@ package com.bestpro.qrscannergenerator.MainFragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,10 +17,13 @@ import androidx.fragment.app.Fragment;
 import com.bestpro.qrscannergenerator.BuildConfig;
 import com.bestpro.qrscannergenerator.R;
 
+import org.w3c.dom.Text;
+
 public class RateUsFragment extends Fragment {
 
     private View v;
-    TextView permission, version;
+    final String appPackageName = BuildConfig.APPLICATION_ID;
+    TextView permission, version, rate_us, share_url;
 
     @Nullable
     @Override
@@ -26,6 +32,8 @@ public class RateUsFragment extends Fragment {
         getActivity().setTitle("Rate us");
         permission = (TextView) v.findViewById(R.id.permission);
         version = (TextView) v.findViewById(R.id.version);
+        rate_us = (TextView) v.findViewById(R.id.rate_us);
+        share_url = (TextView) v.findViewById(R.id.share_url);
         version.setText("Version " + String.valueOf(BuildConfig.VERSION_NAME));
 
         permission.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +55,28 @@ public class RateUsFragment extends Fragment {
                 alert1.show();
             }
         });
+
+        share_url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.bestpro.qrscannergenerator");
+                startActivity(Intent.createChooser(sharingIntent, "Share"));
+            }
+        });
+
+        rate_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        });
+
         return v;
     }
 }
