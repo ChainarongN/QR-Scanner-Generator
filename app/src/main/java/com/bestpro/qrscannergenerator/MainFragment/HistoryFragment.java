d5b12474fa694bb62d89ffd1bb8c1f19e4d9c4dc
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-05-10 09:20:56 +0700

    update View Pager

--- app/src/main/java/com/bestpro/qrscannergenerator/MainFragment/HistoryFragment.java
+++ app/src/main/java/com/bestpro/qrscannergenerator/MainFragment/HistoryFragment.java
@@ -14,13 +14,20 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import com.bestpro.qrscannergenerator.Adapter.SectionsPageAdapter;
 import com.bestpro.qrscannergenerator.BuildConfig;
 import com.bestpro.qrscannergenerator.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class HistoryFragment extends Fragment {
 
     private View v;
+
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Nullable
     @Override
@@ -28,8 +35,24 @@ public class HistoryFragment extends Fragment {
         v = inflater.inflate(R.layout.fragment_history, container, false);
         getActivity().setTitle("History");
 
-        Toast.makeText(getContext(),"aaaaaaa",Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(getContext(), "aaaaaaa", Toast.LENGTH_SHORT).show();
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getFragmentManager());
+
+        mViewPager = (ViewPager) v.findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) v.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
         return v;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getFragmentManager());
+        adapter.addFragment(new TabGenerator(), "Generator");
+        adapter.addFragment(new TabScanner(), "Scanner");
+        viewPager.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/bestpro/qrscannergenerator/MainFragment/TabGenerator.java
@@ -0,0 +1,25 @@
+package com.bestpro.qrscannergenerator.MainFragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.bestpro.qrscannergenerator.R;
+
+public class TabGenerator extends Fragment {
+
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_tab_generator, container, false);
+
+        return v;
+    }
+}
